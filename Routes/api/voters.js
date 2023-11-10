9175ca370b439551b91f27337e839e367b822161
tomide <tomideadeyanju@gmail.com>
2023-11-10 13:29:58 +0100

    changes made

--- Routes/api/voters.js
+++ Routes/api/voters.js
@@ -19,12 +19,13 @@ router.get('/:id', (req, res) => {//get users based on api
 
 
 router.get('/:name', (req, res) => {//get users based on api
-    const found = voters.some(voters => voters.name === req.params.name)// parseInt cannot be used because name is a string not a numeric value
-    if(found)
-    {
-    res.json(voters.filter(voters => voters.name === req.params.name))
-    }else{
-        res.sendStatus(400)
+    const voterName = req.params.name;
+    const foundVoter = voters.find(voter => voter.name === voterName);
+  
+    if (foundVoter) {
+      res.json(foundVoter);
+    } else {
+      res.status(404).json({ error: 'Voter not found' });
     }
 })
 
@@ -35,11 +36,46 @@ router.post('/', (req, res) => {
         name: req.body.name,
         email: req.body.email
     }
-    if(!newVoters.name||!newVoter.email)
+    if(!newVoter.name || !newVoter.email)
     {
         return res.sendStatus(400)
     }
     voters.push(newVoter)
     res.json(voters)
 })
+
+//update voters by selecting id
+router.put('/:id', (req, res) => {
+    const found = voters.some(voters => voters.id === parseInt(req.params.id))// some is used as it satisfy the constraint if any as an ID 
+   if(found)
+   {
+       const updateVoter = req.body;
+       voters.forEach(voters => {
+           if(voters.id === parseInt(req.params.id))
+           {
+               voters.name = updateVoter.name ? updateVoter.name: voters.name
+               voters.email = updateVoter.email? updateVoter.email: voters.email
+               res.json({
+                   message: 'voter Updated',
+                   voters
+               })
+           }
+       })
+   }
+})
+
+//remove a voter by selecting id
+router.delete('/:id', (req, res)=> {
+    const found = voters.some(voters =>  voters.id === parseInt(req.params.id))
+    if(found){
+        voters = voters.filter(voters => voters.id !==  parseInt(req.params.id))
+        res.json({
+            message: 'voters deleted',
+           voters
+        })
+    }else {
+        res.sendStatus(400)
+    }
+})
+
 module.exports = router
